3df7f7d4b4faa42ba6b9d406fc143d10d3753b33
Jussi Piirainen <j.piirainen@hotmail.fi>
2016-03-21 15:08:03 +0200

    first implementation or reconnecting

--- src/app.js
+++ src/app.js
@@ -8,16 +8,50 @@ var commands = require("./commands.js");
 var logger = require("./logger.js");
 
 // Command listener
-bot.on("message", function(message){
+bot.on("message", function(message) {
     logger.log(message);
 	if (message.sender.username != bot.user.username) {
         commands.command(message);
 	}
 });
 
+// Disconnect listener
+bot.on("disconnected", function() {
+    logger.log("Lost connection to server.");
+    
+});
+
+// Reconnects every 15 seconds until login is succesful
+function reconnect() {
+    var reconnecting = setInterval(function() {
+        if (login()) {
+            console.log("Logged in succesfully.")
+            clearTimeout(reconnection);
+        }
+        else {
+            console.log("Reconnecting failed. Trying again in 15 seconds.");
+        }
+    }, 15000);
+}
+
+// Function for logging in with information from config file
+// Returns true if login was successful
+function login() {
+    bot.login(config.discordUsername, config.discordPassword, function(error, token) {
+        if (error) {
+            console.log("Couldn't connect to server :" + error)
+            return false;
+        } else {
+            return true;
+        }
+    });
+}
+
+if (!login()) {
+    reconnect();
+} else {
+    console.log("Logged in successfully.");
+}
+
 // bot-getter
 exports.bot = bot;
-
-bot.login(config.discordUsername, config.discordPassword, function(error, token) {
-    if (error) console.log(error);
-});
