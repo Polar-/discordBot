4e1d8ad60b421d644f956e9da9d66ad4b33d0e9b
Jussi Piirainen <j.piirainen@hotmail.fi>
2016-07-27 20:10:50 +0300

    reintroduced !start which moves players to correct voice channels (only works for players currently connected to any voice channel in the server)

--- src/tenman.js
+++ src/tenman.js
@@ -5,6 +5,7 @@
 var app = require('./app.js');
 var commands = require("./commands.js");
 var config = require("./config/config.js");
+var logger = require("./logger.js");
 
 // for 10mans
 var players = [];
@@ -19,17 +20,15 @@ var cmds = [
             if (commands.isAdmin(message)) {
                 // copy players from options
                 var opt = commands.splitCmd(message.content, 1);
-                console.log(opt);
+
                 opt = opt.split(" ");
                 if (opt.length > 1) {
-                    opt.splice(opt.length - 1, 1);
+                    opt.splice(opt.length, 1);
                 }
-                console.log(opt);
                 
                 if (opt != undefined) {
                     for (var i = 0; i < opt.length; i++) {
-                        console.log(opt[i]);
-                        players.push(opt[i]);
+                        players.push({ name: opt[i] });
                     }
                     commands.sendMessage(message, "Player count: " + players.length + ".");
                 }
@@ -38,13 +37,12 @@ var cmds = [
     },
     {
         cmd: "!resetplayers",
-        alias: "!resetPlayers",
+        alias: "!resetteams",
         execute: function(message) {
-            if (commands.isAdmin(message)) {
-                // wipe players
-                players = [];
-                commands.sendMessage(message, "Players reset.");
-            }
+            if (!commands.isAdmin(message)) { return; };
+            // wipe players
+            players = [];
+            commands.sendMessage(message, "Players reset.");
         }
     },
     {
@@ -55,7 +53,7 @@ var cmds = [
                 if (players != undefined && players.length != 0) {
                     var content = "";
                     for (var i = 0; i < players.length; i++) {
-                        content += players[i] + " ";
+                        content += players[i].name + " ";
                     }
                     commands.sendMessage(message, "Current players (" + players.length + "): " + content);
                 } else {
@@ -74,15 +72,17 @@ var cmds = [
                     var tmpTeam1 = [];
                     var tmpTeam2 = [];
                     var done = 1;
+
                     
                     // random teams
                     while (tmpPlayers.length > 0) {
                         var random = Math.floor(Math.random() * tmpPlayers.length);
-                        var value = tmpPlayers.slice(random, random + 1);
+                        var value = tmpPlayers.slice(random, random + 1)[0];
+                        
                         if (done > 5) {
-                            tmpTeam1.push(value);
+                            tmpTeam1.push(value.name);
                         } else {
-                            tmpTeam2.push(value);
+                            tmpTeam2.push(value.name);
                         }
                         tmpPlayers.splice(random, 1);
                         done++;
@@ -101,10 +101,9 @@ var cmds = [
         execute: function(message) {
             if (commands.isAdmin(message)) {
                 if (team1.length > 0 && players.length != 0) {
-                    console.log(team1);
                     showTeams(message);
                 } else {
-                    commands.sendMessage(message, "Teams haven't been randomed.");
+                    commands.sendMessage(message, "Teams haven't been randomized.");
                 }
             }
         }
@@ -112,50 +111,95 @@ var cmds = [
     {
         cmd: "!start",
         execute: function(message) {
-            // MOVING USERS IN VOICE CHANNELS IS NOT SUPPORTED IN DISCORD.IO
-            /*if (commands.isAdmin(message)) {
-                if (players.length == 10 && team1.length == 5 && team2.length == 5) {
-                    // get voice channel names (from config)
-                    var channels = message.channel.server.channels;
-                    var team1Channel = channels.get("name", config.team1);
-                    var team2Channel = channels.get("name", config.team2);
-                    
-                    // check that channels are set and exist
-                    if (team1Channel == undefined || team2Channel == undefined) {
-                        commands.sendMessage(message, "Team channels not set (or not found), please set them in the config.");
-                    } else {
-                        // send start message, teams
-                        commands.sendMessage(message, "Starting CS:GO 10-man. \nconnect polar.dy.fi; password apina");
-                        
-                        // get user-objects of players
-                        var users = message.channel.server.members;
-                        
-                        for (var i = 0; i < players.length; i++) {
-                            // Find current player from users
-                            var curPlayer = users.get("username", players[i]);
-                            if (curPlayer != undefined) {
-                                // get player team
-                                for (var j = 0; j < team1.length; j++) {
-                                    if (curPlayer.username.toLowerCase() == team1[j].toLowerCase()) {
-                                        // move player
-                                        app.bot.moveMember(curPlayer, team1Channel);
-                                        break;
-                                    }
-                                }
-                                for (var j = 0; j < team2.length; j++) {
-                                    if (curPlayer.username.toLowerCase() == team2[j].toLowerCase()) {
-                                        // move player
-                                        app.bot.moveMember(curPlayer, team2Channel);
-                                        break;
-                                    }
-                                }
-                            }
-                        }
+            // Moves players to voice channels (only works if player is connected to a voice channel) 
+            if (!commands.isAdmin(message)) { return; }
+            if (players.length != 10 || team1.length != 5 || team2.length != 5) {
+                commands.sendMessage(message, "No players.");
+                return;
+            }
+            // Get ID's for all players
+            for (var i = 0; i < players.length; i++) {
+                for (var user in app.bot.users) {
+                    if (app.bot.users[user].username.toLowerCase() == players[i].name.toLowerCase()) {
+                        players[i].id = app.bot.users[user].id;
                     }
-                } else {
-                    commands.sendMessage(message, "No players.");
                 }
-            }*/
+            }
+            // Init server/channel -variables
+            var srv;
+            var ch1;
+            var ch2;
+
+
+            // Get server the command was sent in 
+            for (var i = 0; i < app.bot.info.servers.length; i++) {
+                for (var j = 0; j < app.bot.info.servers[i].channels.length; j++) {
+                    if (app.bot.info.servers[i].channels[j].id == message.channelID) {
+                        srv = app.bot.info.servers[i];
+                    }
+                }
+            }
+
+            // Return if server was not found
+            if (srv == undefined) { return; }
+
+            //get channels
+            for (var i = 0; i < srv.channels.length; i++) {
+                if (srv.channels[i].name == config.team1) {
+                    ch1 = srv.channels[i].id;
+                } else
+                if (srv.channels[i].name == config.team2) {
+                    ch2 = srv.channels[i].id;
+                }
+            }
+            
+            // Return if either channel was not found
+            if (ch1 == undefined || ch2 == undefined) { return; }
+
+            // check that channels are set and exist
+            if (config.team1 == undefined || config.team2 == undefined) {
+                commands.sendMessage(message, "Team channels not set (or not found), please set them in the config.");
+                return;
+            }
+            // send start message, teams
+            commands.sendMessage(message, "Starting CS:GO 10-man. \nconnect polar.dy.fi; password apina");
+            
+            for (var i = 0; i < players.length; i++) {
+                // Find current player from users
+                // get player team
+                for (var j = 0; j < team1.length; j++) {
+                    if (players[i].name.toLowerCase() == team1[j].toLowerCase()) {
+                        // move player
+                        console.log("serv: " + srv.id)
+                        console.log("ch: " + ch1)
+                        console.log("user: " + players[i].id)
+                        app.bot.moveUserTo({
+                            serverID: srv.id,
+                            channelID: ch1,
+                            userID: players[i].id,
+                        });
+                        break;
+                    }
+                }
+                for (var j = 0; j < team2.length; j++) {
+                    if (players[i].name.toLowerCase() == team2[j].toLowerCase()) {
+                        // move player
+                        console.log("serv: " + srv.id)
+                        console.log("ch: " + ch1)
+                        console.log("user: " + players[i].id)
+                        app.bot.moveUserTo({
+                            serverID: srv.id,
+                            channelID: ch2,
+                            userID: players[i].id,
+                        }, function(error) {
+                            if (error) {
+                                logger.log("Error moving user: " + error.message)
+                            }
+                        });
+                        break;
+                    }
+                }
+            }
         }
     }
 ];
